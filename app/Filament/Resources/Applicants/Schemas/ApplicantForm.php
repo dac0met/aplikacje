ea6c304fd1ca59d3f2b4e7fd8c30ad3a57d7eaca
dac0met <jarek@dacomet.pl>
2025-09-23 20:57:11 +0200

    'Filament resources' 	new file:   app/Filament/Resources/Applicants/ApplicantResource.php 	new file:   app/Filament/Resources/Applicants/Pages/CreateApplicant.php 	new file:   app/Filament/Resources/Applicants/Pages/EditApplicant.php 	new file:   app/Filament/Resources/Applicants/Pages/ListApplicants.php 	new file:   app/Filament/Resources/Applicants/Schemas/ApplicantForm.php 	new file:   app/Filament/Resources/Applicants/Tables/ApplicantsTable.php 	new file:   app/Filament/Resources/JobPositions/JobPositionResource.php 	new file:   app/Filament/Resources/JobPositions/Pages/CreateJobPosition.php 	new file:   app/Filament/Resources/JobPositions/Pages/EditJobPosition.php 	new file:   app/Filament/Resources/JobPositions/Pages/ListJobPositions.php 	new file:   app/Filament/Resources/JobPositions/Schemas/JobPositionForm.php 	new file:   app/Filament/Resources/JobPositions/Tables/JobPositionsTable.php 	modified:   app/Models/Applicant.php 	new file:   app/Models/JobPosition.php 	deleted:    app/Models/Job_position.php 	modified:   composer.lock

---
+++ app/Filament/Resources/Applicants/Schemas/ApplicantForm.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Filament\Resources\Applicants\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\DateTimePicker;
+
+class ApplicantForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('submission_id')
+                    ->numeric()
+                    ->default(null),
+                TextInput::make('job_position_id')
+                    ->required()
+                    ->numeric(),
+                DatePicker::make('submitted_date'),
+                TextInput::make('user_ip')
+                    ->default(null),
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('surname')
+                    ->required(),
+                TextInput::make('yob')
+                    ->numeric()
+                    ->default(null),
+                TextInput::make('city')
+                    ->default(null),
+                TextInput::make('phone')
+                    ->tel()
+                    ->numeric()
+                    ->default(null),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->default(null),
+                TextInput::make('consent')
+                    ->default(null),
+                TextInput::make('job_position')
+                    ->default(null),
+                TextInput::make('education')
+                    ->default(null),
+                Textarea::make('university')
+                    ->default(null)
+                    ->columnSpanFull(),
+                Textarea::make('field_of_study')
+                    ->default(null)
+                    ->columnSpanFull(),
+                TextInput::make('english')
+                    ->default(null),
+                TextInput::make('another_lang')
+                    ->default(null),
+                TextInput::make('another_level')
+                    ->default(null),
+                Textarea::make('experience')
+                    ->default(null)
+                    ->columnSpanFull(),
+                TextInput::make('shift_work')
+                    ->required()
+                    ->default(''),
+                TextInput::make('salary')
+                    ->numeric()
+                    ->default(null),
+                TextInput::make('cv_pl')
+                    ->default(null),
+                TextInput::make('cv_gb')
+                    ->default(null),
+                TextInput::make('status')
+                    ->default(null),
+                TextInput::make('english_rating')
+                    ->default(null),
+                TextInput::make('info')
+                    ->default(null),
+                TextInput::make('sent_to')
+                    ->default(null),
+                TextInput::make('interview')
+                    ->default(null),
+                TextInput::make('feedback')
+                    ->default(null),
+                TextInput::make('gender')
+                    ->default(null),
+                TextInput::make('gross')
+                    ->default(null),
+                TextInput::make('consent_source')
+                    ->default(null),
+                TextInput::make('notes')
+                    ->default(null),
+            ]);
+    }
+}
--- app/Models/Applicant.php
+++ app/Models/Applicant.php
@@ -6,5 +6,47 @@ use Illuminate\Database\Eloquent\Model;
 
 class Applicant extends Model
 {
-    //
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'submission_id',
+        'job_position_id',
+        'submitted_date',
+        'user_ip',
+        'name',
+        'surname',
+        'yob',
+        'city',
+        'phone',
+        'email',
+        'consent',
+        'job_position',
+        'education',
+        'university',
+        'field_of_study',
+        'english',
+        'another_lang',
+        'another_level',
+        'experience',
+        'shift_work',
+        'salary',
+        'cv_pl',
+        'cv_gb',
+        'status',
+        'english_rating',
+        'info',
+        'sent_to',
+        'interview',
+        'feedback',
+        'gender',
+        'gross',
+        'consent_source',
+        'notes',
+    ];
+
+    
 }
